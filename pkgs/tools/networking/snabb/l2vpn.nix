da03fa20bfa5a873718d2bfa5eca80ed7e6d1dce
Alexander Gall <alexander.gall@gmail.com>
2016-08-18 18:03:40 +0200

    Bump l2vpn package version to l2vpn-v4
    (cherry picked from commit 20583e16abe12ab2abe3ff1d3b78f134efa409d9)

--- pkgs/tools/networking/snabb/l2vpn.nix
+++ pkgs/tools/networking/snabb/l2vpn.nix
@@ -6,13 +6,13 @@ with pkgs;
 ## "l2vpn" topic branch.
 snabb.overrideDerivation (origAttrs: rec {
   name = "snabb-${version}";
-  version = "l2vpn-v3";
+  version = "l2vpn-v4";
 
   src = fetchFromGitHub {
     owner = "snabbco";
     repo = "snabb";
     rev = "${version}";
-    sha256 = "02in1ni75fkmzwzkk5q0ys2qm8vaf730kcckggwmzmgjzjk8mh0j";
+    sha256 = "11r1dif50kc1qpvvghab5g3is1vb8dpmly6zmk0nwcrlhh24g3yh";
   };
   buildInputs = origAttrs.buildInputs ++ [ git ];
   PREFIX = "./";
