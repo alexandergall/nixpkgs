e020e67b71f4858bcfad06301d818b37c8e12257
Alexander Gall <alexander.gall@gmail.com>
2017-10-23 10:38:08 +0200

    Bump l2vpn package version to l2vpn-v8

--- pkgs/tools/networking/snabb/l2vpn.nix
+++ pkgs/tools/networking/snabb/l2vpn.nix
@@ -9,13 +9,13 @@ with pkgs;
 ##      https://github.com/snabbco/snabb/archive/${version}.tar.gz
 snabb.overrideDerivation (origAttrs: rec {
   name = "snabb-${version}";
-  version = "l2vpn-v7";
+  version = "l2vpn-v8";
 
   src = fetchFromGitHub {
     owner = "snabbco";
     repo = "snabb";
     rev = "${version}";
-    sha256 = "0rh1p3gkdmxzis7wbaawm9cwmd3bv9jx3m6100v6zrgahlx4bfmp";
+    sha256 = "1r8a1qm82zyd7lzzmj8cf0qcydbq99xzgf3sv5aii9ld8jkly9bf";
   };
   buildInputs = origAttrs.buildInputs ++ [ git ];
   PREFIX = "./";
